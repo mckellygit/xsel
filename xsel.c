26d16fcbc447bc67c46ccc1dd0a8b8d2b7becd48
Mark Kelly <mark.kelly@lexisnexisrisk.com>
2019-12-17 00:02:43 -0500

    remove all trailing LF and CR chars if --rmlastnl specified

--- xsel.c
+++ xsel.c
@@ -960,7 +960,7 @@ try_read:
 
   if (total_input) {
     if (rm_lastnl) {
-      if (read_buffer[total_input-1] == '\n') {
+      while( (read_buffer[total_input-1] == '\n') || (read_buffer[total_input-1] == '\r') ) {
         read_buffer[total_input-1] = '\0';
         total_input--;
       }
